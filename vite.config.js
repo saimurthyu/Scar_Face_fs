c60cb87d120f45a4f13c999fefe291bff18d644e
Sai Murthy Uppada <54973387+saimurthyu@users.noreply.github.com>
2026-03-28 13:35:57 +0000

    modified the vite script

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api/apewisdom": {
-        target: "https://apewisdom.io",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/apewisdom/, ""),
-      },
-    },
+    allowedHosts: true,
   },
 });
